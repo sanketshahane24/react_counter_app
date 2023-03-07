7c24b457ee8e4e16e3c31a76154e76fc9e62be6a
Sanket Shahane <shahanesanket24@gmail.com>
2023-03-06 22:54:26 -0800

    react-counter-app added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+  const [count,setCount] = useState(0)
+
+  function decrease(){
+    setCount(c=>c-1)
+  }
+
+  function increase(){
+    setCount(c=>c+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={decrease}>-</button>
+      <span>{count}</span>
+      <button onClick={increase}>+</button>
     </div>
   );
 }
